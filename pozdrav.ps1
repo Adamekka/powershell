17a1e73d808ecadc69b72ebea90417935d81f72f
Adam Cvikl <adam.wasd4@gmail.com>
2023-02-21 15:19:25 +0100

    create pozdrav.ps1

---
+++ pozdrav.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env pwsh
+
+function nezdrav {
+    Write-Output "a nepozdravi"
+}
+
+function pozdrav {
+    Write-Output "Ahoj"
+}
+
+Write-Output "Tento script vas nepozdravi"
+nezdrav
