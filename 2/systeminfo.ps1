679f8966efc8c014a79f7bd45bbf4e40fac093f7
Adam Cvikl <adam.wasd4@gmail.com>
2023-04-18 15:24:48 +0200

    improve `systeminfo.ps1`

--- 2/systeminfo.ps1
+++ 2/systeminfo.ps1
@@ -9,15 +9,6 @@ $date_time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 # Get info about logged in user
 $logged_in_user = Get-Process -Id $pid -IncludeUserName | Select-Object -ExpandProperty UserName
 
-# Get system info
-$system_info = [ordered]@{}
-$system_info["computer_name"] = $env:COMPUTERNAME
-$system_info["os"] = $env:OS
-$system_info["arch"] = $env:PROCESSOR_ARCHITECTURE
-$system_info["system_drive"] = $env:SystemDrive
-$system_info["system_directory"] = $env:SystemRoot
-$system_info["user"] = $env:USERNAME
-$system_info["user_profile"] = $env:USERPROFILE
 
 # Get path info
 $path_info = [ordered]@{}
@@ -27,8 +18,7 @@ $path_info["temp"] = $env:TEMP
 $path_info["tmp"] = $env:TMP
 
 # Get env variables
-$env_variables = [ordered]@{}
-$env_variables["env"] = $env:env
+$env_variables = Get-ChildItem -Path Env:
 
 # Delete previout systeminfo.txt if it exists
 if (Test-Path "systeminfo.txt") {
@@ -41,7 +31,7 @@ $date_time | Out-File -FilePath "systeminfo.txt" -Append
 "`nLogged in user:`n" | Out-File -FilePath "systeminfo.txt" -Append
 $logged_in_user | Out-File -FilePath "systeminfo.txt" -Append
 "`nSystem info:" | Out-File -FilePath "systeminfo.txt" -Append
-$system_info | Out-File -FilePath "systeminfo.txt" -Append
+systeminfo | Out-File -FilePath "systeminfo.txt" -Append
 "`nPath info:" | Out-File -FilePath "systeminfo.txt" -Append
 $path_info | Out-File -FilePath "systeminfo.txt" -Append
 "`nEnv variables:" | Out-File -FilePath "systeminfo.txt" -Append
